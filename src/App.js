486ad8f4358d906b55042dd0551db7aa66c465c5
Niway <cniway94@gmail.com>
2025-03-20 17:12:30 +0300

    update the cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <>
-    <div>hello there.</div>
-    <h1>Niway</h1></>
-  )
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
