12ccaa90080131d381587c7f9b651f0c2b93d11f
Niway <cniway94@gmail.com>
2025-03-22 09:56:56 +0300

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import SimpleInput from "./components/SimpleInput";
+import BasicForm from "./components/BasicForm";
 
 function App() {
   return <div>
-    <SimpleInput/>
+    <BasicForm/>
   </div>;
 }
 
